e7f8ec45d63c305e55dd60a033077510ac59f594
Steve Polito <info@stevepolitodesign.com>
2019-05-14 08:34:00 -0400

    add todo test

--- __tests__/response.js
+++ __tests__/response.js
@@ -1,12 +1,13 @@
-const Response = require('./../lib/response')
+const Response = require('../lib/response')
 
 describe('#getResponse', () => {
-    let response
     test('should throw an error if there is no url', () => {
-        response = new Response()
+        const response = new Response()
         expect(()=>{
             response.getResponse()
         }).toThrow()
     })
+
+    test.todo('should return a status code')
 
 })
