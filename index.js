40a25534c5d306544264246eee50f5e1525f5782
Steve Polito <info@stevepolitodesign.com>
2019-06-25 13:57:03 -0400

    install prettier

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const sites = require('./lib/sites')
-const Response = require('./lib/response')
+const sites = require("./lib/sites");
+const Response = require("./lib/response");
 
 sites.forEach(site => {
-    const { url, emails } = site
-    let siteResponse = new Response(url, emails)
-    siteResponse.handleResponse()
-})
+  const { url, emails } = site;
+  let siteResponse = new Response(url, emails);
+  siteResponse.handleResponse();
+});
